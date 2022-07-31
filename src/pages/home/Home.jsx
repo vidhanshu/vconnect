870a5d4f28c46e3f66eb33f056997069ff0ee9ad
vidhanshu <vidhanshu7218555039@gmail.com>
2022-07-31 15:14:12 +0530

    font size overflow fixe

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,20 +4,22 @@ import { Header, Posts, Section } from "../../components";
 
 function Home() {
   return (
-    <div className={styles.homeContainer}>
+    <>
       <Header />
-      <Section>
-        <div className={styles.createPost}>
-          <p>Create a post</p>
-          <button>New</button>
-        </div>
-      </Section>
-      <Section>
-        <div className={styles.posts}>
-          <Posts />
-        </div>
-      </Section>
-    </div>
+      <div className={styles.homeContainer}>
+        <Section>
+          <div className={styles.createPost}>
+            <p>Create a post</p>
+            <button>New</button>
+          </div>
+        </Section>
+        <Section>
+          <div className={styles.posts}>
+            <Posts />
+          </div>
+        </Section>
+      </div>
+    </>
   )
 }
 
